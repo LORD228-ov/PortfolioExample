ff20de8bb7e045b5f11dff7f650db3a389a7385c
LORD228 <ggvoleg228@gmail.com>
2026-06-26 16:23:42 +0200

    Refactor: New projects & Info

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,13 @@
 fetch("scripts/data.json")
     .then(res => res.json())
     .then(data => {
-
-        // document.getElementById("skills-title").textContent = data.skillsTitle;
-
-        // const softSkillsBox = document.getElementById("soft-skills");
-        // const progSkillsBox = document.getElementById("programming-skills");
-
-        // softSkillsBox.innerHTML = `<div><hr style="border-top: 5px solid rgb(255, 255, 255); width: 80%; margin: 10px auto;"></div>`;
-        // progSkillsBox.innerHTML = `<div><hr style="border-top: 5px solid rgb(255, 255, 255); width: 80%; margin: 10px auto;"></div>`;
-
-        // data.softSkills.forEach(skill => {
-        //     const p = document.createElement("p");
-        //     p.className = "fw-bold";
-        //     p.textContent = skill;
-        //     softSkillsBox.appendChild(p);
-        // });
-
-        // // Загружаем programming skills
-        // data.programmingSkills.forEach(skill => {
-        //     const p = document.createElement("p");
-        //     p.className = "fw-bold";
-        //     p.textContent = skill;
-        //     progSkillsBox.appendChild(p);
-        // });
-
         document.getElementById("projects-title").textContent = data.projectsTitle;
         const container = document.getElementById("projects-container");
         container.innerHTML = "";
 
         data.projects.forEach(project => {
             const col = document.createElement("div");
-            col.className = "col-sm-6 col-lg";
+            col.className = "col-12 col-md-6";
 
             col.innerHTML = `
                 <a href="${project.link}" target="_blank" class="text-decoration-none text-light">
